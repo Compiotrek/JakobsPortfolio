9217aa1a941e4eade8790ba7ff4586c6b3540189
Jakob Karsten <karsten@campus.tu-berlin.de>
2026-05-18 14:05:14 +0200

    directory changed

--- src/data/FileSystem.js
+++ src/data/FileSystem.js
@@ -5,13 +5,6 @@ const FileSystem = {
       Documents: {
         type: "directory",
         children: {
-          "index.html": { type: "file", page: "home" },
-          "about.html": { type: "file", page: "about" },
-          "projects.html": { type: "file", page: "projects" },
-          "LinearRegression.html": {
-            type: "file",
-            page: "linear-regression",
-          },
           "document-workflow-intelligence.html": {
             type: "file",
             page: "document-workflow-intelligence",
